77b3d83434af88a8edbf8dc08c0a2796a43b722f
zotov88 <zotov_l88@mail.ru>
2022-04-19 15:15:47 +0300

    Add gitignore

---
+++ For.java
@@ -0,0 +1,12 @@
+public class For {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        System.out.println(add(a, b));
+
+    }
+    private static int add(int a, int b) {
+        return a + b;
+    }
+}
